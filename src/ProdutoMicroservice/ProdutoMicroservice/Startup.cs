bb09155e326df37b8c0931bc9c300c412613f4b8
Jose Augusto <josea@MOST.COM>
2019-08-07 17:42:46 -0300

    compilação no vs code e adição do swagger

--- src/ProdutoMicroservice/ProdutoMicroservice/Startup.cs
+++ src/ProdutoMicroservice/ProdutoMicroservice/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using ProdutoMicroservice.Models;
 using ProdutoMicroservice.Services;
+using Microsoft.OpenApi.Models;
 
 namespace ProdutoMicroservice
 {
@@ -35,6 +36,12 @@ namespace ProdutoMicroservice
             services.AddSingleton<ProdutoService>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Teste API", Version = "v1" });
+            });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +51,15 @@ namespace ProdutoMicroservice
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+            
 
             app.UseMvc();
         }
